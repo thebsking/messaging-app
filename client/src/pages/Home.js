80a13901a94b0d097878e2335d6a5880b207f30e
Brandon <codebybk@gmail.com>
2021-12-17 16:46:30 -0500

    added placeholder home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    return(
+        <>
+            <main>
+                <h1 id='man-page-text'>Incoming message... from the <em>FUTURE</em>!</h1>
+            </main>
+        </>
+    )
+}
